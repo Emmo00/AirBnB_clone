48a50aa1c9c9d2827502c23c91c1b3991cf9a2a8
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-10 22:50:32 +0100

    test baseModel

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,41 @@
+from time import sleep
+import unittest
+import sys
+
+sys.path.insert(0, '../../')
+
+from models.base_model import BaseModel
+from datetime import datetime
+
+class TestBaseModelClass(unittest.TestCase):
+    def test_unique_id(self):
+        model1 = BaseModel()
+        model2 = BaseModel()
+        self.assertFalse(model1.id == model2.id)
+
+    def test_id_type(self):
+        model = BaseModel()
+        self.assertIsInstance(model.id, str)
+
+    def test_created_updated_type(self):
+        model = BaseModel()
+        self.assertIsInstance(model.created_at, datetime)
+        self.assertIsInstance(model.updated_at, datetime)
+
+    def test_str_representation(self):
+        model = BaseModel()
+        self.assertIsInstance(model.__str__(), str)
+
+    def test_updated_after_save(self):
+        model = BaseModel()
+        update_time = model.updated_at
+        sleep(2)
+        model.save()
+        self.assertNotEqual(update_time, model.updated_at)
+
+    def test_to_dict(self):
+        model = BaseModel()
+        model_dict = model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], model.__class__.__name__)
+        self.assertIsInstance(model_dict['created_at'], str)
