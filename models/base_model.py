b222328edbee17a7fb779331a0ccfaedaa78ddc8
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-10 22:49:54 +0100

    spec attrs

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,18 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    self.__setattr__(key, value)
+            self.created_at = datetime.fromisoformat(self.created_at)
+            self.updated_at = datetime.fromisoformat(self.updated_at)
+            return
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-    
+
     def __str__(self):
         return "[{}] ({}) {}".format(
             self.__class__.__name__,
