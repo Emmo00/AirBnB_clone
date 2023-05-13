d034987ec0f36f6ef31ec945187fd42d3d4c3fa7
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-13 16:47:18 +0100

    classes

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if len(kwargs) != 0:
+            self.state_id = ""
+            self.name = ""
