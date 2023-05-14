fdfa58dba1e4aa752fed321839381b0295f825b0
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-14 17:15:30 +0100

    import new classes

--- models/base_model.py
+++ models/base_model.py
@@ -33,7 +33,6 @@ class BaseModel:
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.now()
-        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
