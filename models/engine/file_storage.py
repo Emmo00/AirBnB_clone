151de0ce3449b1291815fadc14641f7f4812924b
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-05-14 06:21:33 -0700

    documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Defines the FileStorage class."""
 import json
 import os
 from ..base_model import BaseModel
@@ -6,23 +7,23 @@ from ..user import User
 
 
 class FileStorage:
-    """Represent a storage engine that that serializes instances to a JSON file
+    """storage engine that that serializes instances to a JSON file
     and deserializes JSON file to instances"""
     def __init__(self):
         self.__file_path = 'file.json'
         self.__objects = {}
-    
+
     def all(self):
         """Return the dictionary __objects."""
         return self.__objects
-    
+
     def new(self, obj):
-         """Set in __objects obj with key <obj_class_name>.id"""
+        """Set in __objects obj with key <obj_class_name>.id"""
         self.__objects["{}.{}".format(
             obj.__class__.__name__,
             obj.id
         )] = obj
-    
+
     def save(self):
         """Serialize __objects to the JSON file __file_path."""
         with open(self.__file_path, 'w') as f:
@@ -30,10 +31,10 @@ class FileStorage:
             for key, value in self.__objects.items():
                 to_save[key] = value.to_dict()
             json.dump(to_save, f)
-    
+
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
-        if os.path.exists(self.__file_path) == False:
+        if not os.path.exists(self.__file_path):
             return
         with open(self.__file_path, 'r') as f:
             self.__objects = json.load(f)
