fe0333e94914e888da9b1cf23489a7e0665fbd7d
Tosinvee <bayodeoluwatosin06@gmail.com>
2023-05-14 11:20:11 -0700

    documentation

--- models/city.py
+++ models/city.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""Defines the City class."""
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
+    """Represent a city.
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if len(kwargs) != 0:
--- models/user.py
+++ models/user.py
@@ -1,7 +1,16 @@
+#!/usr/bin/python3
+"""Defines the User class."""
 from .base_model import BaseModel
 
 
 class User(BaseModel):
+    """Represent a User.
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         if len(kwargs) == 0:
