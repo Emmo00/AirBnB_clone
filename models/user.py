73e964d7b6c83b480036f075ff1f9d2bc6dc3bc1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-15 05:19:28 -0700

    pub class att

--- models/user.py
+++ models/user.py
@@ -8,10 +8,7 @@ from .base_model import BaseModel
 class User(BaseModel):
     """User class.
     """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if len(kwargs) == 0:
-            self.email = ""
-            self.password = ""
-            self.first_name = ""
-            self.last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
